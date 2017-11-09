ec27460d70c75b6ed59dbd2324b0514dd8a9fc98
Mathew Calkins <mathewpcalkins@gmail.com>
2017-11-08 20:48:54 -0500

    Access and consumer tokens all set up

--- collectData.py
+++ collectData.py
@@ -1,2 +1,21 @@
 import tweepy
-print("hello world!")
+from tweepy import OAuthHandler
+import sys
+sys.path.insert(0,'/home/mathew')
+import twitter_secret
+
+consumer_key = '6SSlw3YCpX0kgT7U3rPKixC1M'
+consumer_secret = twitter_secret.consumer_secret
+access_token = twitter_secret.access_token
+access_secret = twitter_secret.access_secret
+ 
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+ 
+api = tweepy.API(auth)
+
+# print("hello world")
+print(consumer_secret)
+#for status in tweepy.Cursor(api.home_timeline).items(1):
+    # Process a single status
+    #print(status.text)
